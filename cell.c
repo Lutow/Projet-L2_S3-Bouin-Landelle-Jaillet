edf78e7f1019aec26e7626767c996b1d0463d171
matthieu86 <126449912+matthieu86@users.noreply.github.com>
2023-12-10 20:03:55 +0100

    Update cell.c

--- cell.c
+++ cell.c
@@ -1,16 +1,14 @@
-//
-// Created by Matthieu on 06/12/2023.
-//
-
 #include "Cell.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function that allows to create a cell with a given value (int) and a given number of levels (int)
+// it returns a new cell of type t_d_cell
 t_d_cell* createCell(int value, int nb_levels){
-    t_d_cell* newCell=(t_d_cell*)malloc(sizeof(t_d_cell));
-    newCell->value=value;
-    newCell->nbLevels=nb_levels;
-    t_d_cell **newCells=(t_d_cell**)malloc(sizeof(t_d_cell*)*nb_levels);
+    t_d_cell* newCell=(t_d_cell*)malloc(sizeof(t_d_cell));               // allocate memory for a new cell
+    newCell->value=value;         // assign the given value
+    newCell->nbLevels=nb_levels;  // Set the number of levels
+    t_d_cell **newCells=(t_d_cell**)malloc(sizeof(t_d_cell*)*nb_levels); // allocate memory for an array of pointers to cells for each level
     newCell->cellsNext.next=newCells;
     for(int i=0; i<nb_levels; i++){
         newCell->cellsNext.next[i]=NULL;
