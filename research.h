43ef20633920d2330108ba4a781f8af6ef5b2f5a
matthieu86 <126449912+matthieu86@users.noreply.github.com>
2023-12-10 18:54:26 +0100

    Create research.h

---
+++ research.h
@@ -0,0 +1,11 @@
+#ifndef PROJET_S3_V2_RESEARCH_H
+#define PROJET_S3_V2_RESEARCH_H
+#include "list.h"
+#include "Cell.h"
+
+int research_classic(t_d_list* myList, int value);
+
+int high_search(int val, t_d_list* lst);
+
+
+#endif
