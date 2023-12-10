ba696b2838936ed64e5b5b9df26ef6ef5d9186ec
matthieu86 <126449912+matthieu86@users.noreply.github.com>
2023-12-10 20:04:21 +0100

    Update cell.h

--- cell.h
+++ cell.h
@@ -1,10 +1,12 @@
 #ifndef PROJET_S3_V2_CELL_H
 #define PROJET_S3_V2_CELL_H
 
+// Structure to create the link between a cell and the following one
 typedef struct Cells{
     struct t_d_cell **next;
 }Cells;
 
+// Structure of a cell
 typedef struct s_d_cell{
     int nbLevels;
     int value;
@@ -13,4 +15,4 @@ typedef struct s_d_cell{
 
 t_d_cell* createCell(int value, int nb_levels);
 
-#endif //PROJET_S3_V2_CELL_H
+#endif
